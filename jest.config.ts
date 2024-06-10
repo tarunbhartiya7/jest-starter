3e732e3463c34817f3c36808a1b12d5e813deae6
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2024-06-10 18:33:17 +0530

    test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,11 @@
-import type { Config } from '@jest/types'
+import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    verbose: true
-}
+  preset: "ts-jest",
+  testEnvironment: "node",
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: ["<rootDir>/src/app/**/*.ts"],
+};
 
 export default config;
