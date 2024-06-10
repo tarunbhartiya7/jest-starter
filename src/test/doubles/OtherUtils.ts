4a7f0e0cf0c9a0b798381c3cba75c154a7ec4306
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2024-06-11 00:05:12 +0530

    test doubles

---
+++ src/test/doubles/OtherUtils.ts
@@ -0,0 +1,69 @@
+import {
+  OtherStringUtils,
+  calculateComplexity,
+  toUpperCaseWithCb,
+} from "../../app/doubles/OtherUtils";
+
+describe("OtherUtils test suite", () => {
+  describe("OtherStringUtils tests with spies", () => {
+    let sut: OtherStringUtils;
+
+    beforeEach(() => {
+      sut = new OtherStringUtils();
+    });
+
+    test("Use a spy to track calls", () => {
+      const toUpperCaseSpy = jest.spyOn(sut, "toUpperCase");
+      sut.toUpperCase("asa");
+      expect(toUpperCaseSpy).toHaveBeenCalledWith("asa");
+    });
+
+    test("Use a spy to track calls to other module", () => {
+      const consoleLogSpy = jest.spyOn(console, "log");
+      sut.logString("abc");
+      expect(consoleLogSpy).toHaveBeenCalledWith("abc");
+    });
+
+    test("Use a spy to replace the implementation of a method", () => {
+      jest.spyOn(sut, "callExternalService").mockImplementation(() => {
+        console.log("calling mocked implementation!!!");
+      });
+      sut.callExternalService();
+    });
+  });
+
+  describe("Tracking callbacks with Jest mocks", () => {
+    const callBackMock = jest.fn();
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it("calls callback for invalid argument - track calls", () => {
+      const actual = toUpperCaseWithCb("", callBackMock);
+      expect(actual).toBeUndefined();
+      expect(callBackMock).toHaveBeenCalledWith("Invalid argument!");
+      expect(callBackMock).toHaveBeenCalledTimes(1);
+    });
+
+    it("calls callback for valid argument - track calls", () => {
+      const actual = toUpperCaseWithCb("abc", callBackMock);
+      expect(actual).toBe("ABC");
+      expect(callBackMock).toHaveBeenCalledWith("called function with abc");
+      expect(callBackMock).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  it("Calculates complexity", () => {
+    const someInfo = {
+      length: 5,
+      extraInfo: {
+        field1: "someInfo",
+        field2: "someOtherInfo",
+      },
+    };
+
+    const actual = calculateComplexity(someInfo as any);
+    expect(actual).toBe(10);
+  });
+});
