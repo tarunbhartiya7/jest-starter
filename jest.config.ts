35370cd1bb0f3c526e99a6f132dfc3e0d6549dd3
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2024-06-12 00:20:28 +0530

    integration tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "@jest/types";
 
 const baseDir = "<rootDir>/src/app/server_app";
-const baseTestDir = "<rootDir>/src/test/server_app";
+const baseTestDir = "<rootDir>/src/test/server_app3";
 
 const config: Config.InitialOptions = {
   preset: "ts-jest",
@@ -9,7 +9,7 @@ const config: Config.InitialOptions = {
   verbose: true,
   collectCoverage: true,
   // collectCoverageFrom: ["<rootDir>/src/app/**/*.ts"],
-  collectCoverageFrom: [`${baseDir}/**/*.ts`],
+  // collectCoverageFrom: [`${baseDir}/**/*.ts`],
   testMatch: [`${baseTestDir}/**/*.ts`],
 };
 
